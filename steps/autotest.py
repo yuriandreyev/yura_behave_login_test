5c8a21b8ac7acf3973da03a37fe341e72a5a443d
yuriandreyev <ya_andreev@mail.ru>
2017-08-06 23:55:56 +0600

    Update autotest.py

--- steps/autotest.py
+++ steps/autotest.py
@@ -2,41 +2,32 @@
 
 import page
 from behave import *
-from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
 @given('web page is opened')
-def set_driver(context):
-    """Webdriver initialization"""
-    context.driver = webdriver.Firefox()
-    # driver = webdriver.Chrome()
+def step_impl(context):
+
     context.driver.get('https://www.dispatch.navitel.ru/')
-    # Load the main page. In this case the home page of dispatch.navitel.ru.
-    context.main_page = page.MainPage(context.driver)
-    # Checks if the text "Navitel Dispatch" is in title
-    assert context.main_page.is_title_matches(), "dispatch.navitel.ru title doesn't match."
+    # Checks if the text "Navitel Dispatch" is in the title
+    assert 'Navitel Dispatch' in context.driver.title, "dispatch.navitel.ru title doesn't match."
 
 
-@when('set username, password to fields and press Enter')
-def authorization(context):
+@when('set username "{login}", password "{password}" to fields and press Enter')
+def step_impl(context, login, password):
     """Tests login feature of 'https://www.dispatch.navitel.ru/'. Detects username in the page after login."""
-    context.main_page.username = 'autotest'  # Sending username to username textbox.
-    context.main_page.password = '111111'  # Sending password to password textbox.
-    context.main_page.password = Keys.RETURN  # Press Enter
+    main_page = page.MainPage(context.driver)
+    main_page.username = login  # Sending username to username textbox.
+    main_page.password = password  # Sending password to password textbox.
+    main_page.password = Keys.RETURN  # Press Enter
     # context.main_page.click_login_button()  # Clicking login button does not work
 
 
 @then('user can see his account page')
-def test_userpage(context):
+def step_impl(context):
     """Function tests if authorization was successful"""
     user_page = page.UserPage(context.driver)
     assert user_page.is_login_ok(), "login have not been successful"
-
-
-@then('browser should be closed')
-def close_browser(context):
-    context.driver.close()
 
 
 
